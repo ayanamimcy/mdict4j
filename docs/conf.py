cc3b810c0a87376561ca87b80fb5375bcf7b51d4
Hiroshi Miura <miurahr@linux.com>
2022-07-15 12:04:47 +0900

    docs: update dependency to MyST-parser
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ sys.path.append(os.path.abspath('_extensions'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx_markdown_tables']
+extensions = ['myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
